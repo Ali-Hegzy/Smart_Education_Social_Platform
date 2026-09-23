04db2dc9624d53d17a95368b7ec871f05bd3a016
Ali Hegazy <alihegazy19@outlook.com>
2026-09-23 10:42:36 +0300

    Change the flow of the project from SSR to REST API

--- src/app.js
+++ src/app.js
@@ -1,14 +1,10 @@
-const express = require('express');
 const path = require('path');
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname ,'./resources/views'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname ,'../public')));
 
 app.get('/', (req, res)=>{
-    res.render('index');
 });
 
 module.exports = app;
